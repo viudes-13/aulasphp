54b2208f726de57c8d908802b6588c68a2af5ea9
THIAGO VIUDES CARSADO <129440601+viudes-13@users.noreply.github.com>
2023-05-02 14:45:13 -0300

    Create 01-sintaxe-geral.php

---
+++ 01-sintaxe-geral.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sintaxe geral do PHP</title>
+</head>
+<body>
+    <h1>Experimentando PHP</h1>
+<hr>
+
+<?php
+echo "Olá PHP!";
+echo "teste";
+
+$escola = "Senac";
+
+echo $escola;
+// Comentarios de uma linha CTRL
+/* ALT SHIFT A 
+COMENTARIOS MULTIPLAS LINHAS */
+
+
+$escola = "secanac";
+echo $escola;
+
+?>
+</body>
+</html>
