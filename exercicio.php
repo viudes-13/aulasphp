a6f79ab8a228176aebb6c623d04dc3dcc3059c40
THIAGO VIUDES CARSADO <129440601+viudes-13@users.noreply.github.com>
2023-05-04 14:29:38 -0300

    exercicio 02

---
+++ exercicio.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>exercicio PHP</title>
+</head>
+<body>
+<h1>Exercicio 02</h1>
+<hr>
+<?php
+$dados = [
+    //chave => valor
+    "usuario" => "thiago";
+    "senha"   =>"123";
+    "idade"   =>19;
+    "cidade"  "são paulo"
+]
+?>    
+
+<h2>Saida de dados</h2>
+<ol>
+    <li><?=$dados["usuario"]?></li>
+    <li></li>
+    <li></li>
+</ol>
+</body>
+</html>
